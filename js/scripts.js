5003d20107c73870b28c565d5e08bbadeba70389
Trianglium <jbrockerrudow@gmail.com>
2022-03-11 12:33:20 -0600

    Fix login modal script

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,35 @@
-// Reserve Form Tool Tip
 $(document).ready(function(){
+  // Reserve Form Tool Tip
   $('[data-toggle="tooltip"]').tooltip();
-});
 
+  // Carousel play/pause button
+  $("#carouselButton").click(function(){
+      if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+          $("#mycarousel").carousel('pause');
+          $("#carouselButton").children("span").removeClass('fa-pause');
+          $("#carouselButton").children("span").addClass('fa-play');
+      }
+      else if ($("#carouselButton").children("span").hasClass('fa-play')){
+          $("#mycarousel").carousel('cycle');
+          $("#carouselButton").children("span").removeClass('fa-play');
+          $("#carouselButton").children("span").addClass('fa-pause');
+      }
+  });
 
-// Carousel play/pause button
-$("#carouselButton").click(function(){
-    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
-        $("#mycarousel").carousel('pause');
-        $("#carouselButton").children("span").removeClass('fa-pause');
-        $("#carouselButton").children("span").addClass('fa-play');
-    }
-    else if ($("#carouselButton").children("span").hasClass('fa-play')){
-        $("#mycarousel").carousel('cycle');
-        $("#carouselButton").children("span").removeClass('fa-play');
-        $("#carouselButton").children("span").addClass('fa-pause');
-    }
-});
+  // Login Modal Close Buttons / Toggle
+  $("#loginBtn").click(function(){
+    $("#loginModal").modal({hide: false});
+  });
+  $("#loginClose").click(function(){
+    $("#loginModal").modal({hide: true});
+  });
 
-// Login Modal JS
-$(document).ready(function(){
-    $("#loginBtn").click(function(){
-      $("#loginModal").modal({show: true});
-    });
-    $("#loginClose").click(function(){
-      $("#loginModal").modal({show: false});
-    });
-});
+  // Reserve Table Modal
+  $("#reserveBtn").click(function(){
+    $("#reserveform").modal({hide: false});
+  });
+  $("#reserveClose").click(function(){
+    $("#reserveform").modal({hide: true});
+  });
 
-// Reserve Table Modal JS
-$(document).ready(function(){
-    $("#reserveBtn").click(function(){
-      $("#reserveform").modal({show: true});
-    });
-    $("#reserveClose").click(function(){
-      $("#reserveform").modal({show: false});
-    });
 });
