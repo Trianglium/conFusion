7ff7a677fe9961dacfb7e9a74282fbfc40c2f683
Trianglium <jbrockerrudow@gmail.com>
2022-03-14 11:34:00 -0500

    Configure react

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,4 +1,4 @@
-import React { Component } from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
