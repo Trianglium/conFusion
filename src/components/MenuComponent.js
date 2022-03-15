12729bac8a35f0ab639bb31416d38d47af4d69d6
Trianglium <jbrockerrudow@gmail.com>
2022-03-15 09:46:53 -0500

    Separation of components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import { Media } from 'reactstrap';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-// import DishdetailComponent from './components/DishdetailComponent';
+import DishDetail from './DishdetailComponent';
 
 class Menu extends Component {
-
     constructor(props) {
         super(props);
 
         this.state = {
-            selectedDish: null
+          selectedDish: null
         }
     }
 
@@ -17,29 +16,11 @@ class Menu extends Component {
         this.setState({ selectedDish: dish});
     }
 
-    renderDish(dish) {
-        if (dish != null)
-            return(
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else
-            return(
-                <div></div>
-            );
-    }
-
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-              <div  className="col-12 col-md-5 m-1">
-                <Card key={dish.id}
-                  onClick={() => this.onDishSelect(dish)}>
+              <div className="col-12 col-md-5 m-1">
+                <Card key={dish.id}>
                   <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
                       <CardTitle>{dish.name}</CardTitle>
@@ -50,19 +31,14 @@ class Menu extends Component {
         });
 
         return (
-            <div className="container">
-                <div className="row">
-                    {menu}
-                </div>
-                <div className="row">
-                  <div  className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.state.selectedDish)}
-                  </div>
-                </div>
+          <div className="container">
+            <div className="row">
+                  {menu}
             </div>
+              <DishDetail dish={this.state.selectedDish} />
+          </div>
         );
     }
 }
-
 
 export default Menu;
