648612023aa6771528f2789859fb595ab65e6256
Trianglium <jbrockerrudow@gmail.com>
2022-03-15 06:20:36 -0500

    Add DishDetailComponent to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import DishDetail from './components/DishDetailComponent';
 import { DISHES } from './shared/dishes';
 import './App.css';
 
@@ -22,6 +23,7 @@ class App extends Component {
           </div>
         </Navbar>
         <Menu dishes={this.state.dishes} />
+        <DishDetail dishes={this.state.dishes} />
       </div>
     );
   }
