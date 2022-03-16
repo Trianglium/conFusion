29538955f6000517761f85beabdd2854c9e3f55a
Trianglium <jbrockerrudow@gmail.com>
2022-03-16 09:37:16 -0500

    Single Page Applications part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,10 @@ import Home from './HomeComponent';
 import Contact from './ContactComponent';
 
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
 
 class Main extends Component {
 
@@ -18,13 +22,20 @@ class Main extends Component {
 
     this.state = {
         dishes: DISHES,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
     };
   }
 
   render() {
     const HomePage = () => {
       return(
-          <Home />
+          <Home
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
       );
     }
 
