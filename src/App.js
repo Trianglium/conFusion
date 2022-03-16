a03656f8a832ca935d57cff23f788b9ac4367279
Trianglium <jbrockerrudow@gmail.com>
2022-03-16 08:02:20 -0500

    React Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { BrowserRouter } from 'react-router-dom';
 import Menu from './components/MenuComponent';
 import Main from './components/MainComponent';
 import { DISHES } from './shared/dishes';
@@ -11,9 +12,11 @@ class App extends Component {
 
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <Main />
       </div>
+    </BrowserRouter>
     );
   }
 }
