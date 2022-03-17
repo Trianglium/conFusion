94c0ec58a4bb018acc26b87460cb589814b26e3f
Trianglium <jbrockerrudow@gmail.com>
2022-03-17 11:33:17 -0500

    About Breadcrumbs, Card, Skeleton set up

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,7 +2,41 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+function About(props) {
+  const leaders = props.leaders.map((leader) => {
+    return (
+      <p>Leader {leader.name}</p>
+    );
+  });
+
+  return (
+    <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+          <BreadcrumbItem active>About Us</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>About Us</h3>
+          <hr />
+        </div>
+      </div>
+      <div className="row row-content">
+        <div className="col-12 col-md-6">
+          <h2>Our History</h2>
+          <p></p>
+        </div>
+        <div className="col-12 col-md-5">
+          <Card></Card>
+        </div>
+        <div className="col-12">
+        </div>
+
+      </div>
+    </div>
+  );
+}
 
 
 
-export default About;  
+export default About;
