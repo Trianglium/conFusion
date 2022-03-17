f6fb35e42afe32c2820c0f37b918630487de41f1
Trianglium <jbrockerrudow@gmail.com>
2022-03-17 15:09:27 -0500

    Update imports

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,5 @@
+import React, { Component } from 'react';
 import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem } from 'reactstrap';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -43,7 +43,7 @@ class Header extends Component {
                         </Collapse>
                     </div>
                 </Navbar>
-                <Jumbotron>
+                <div className="jumbotron">
                     <div className="container">
                         <div className="row row-header">
                             <div className="col-12 col-sm-6">
@@ -52,7 +52,7 @@ class Header extends Component {
                             </div>
                         </div>
                     </div>
-                </Jumbotron>
+                </div>
             </div>
         );
     }
