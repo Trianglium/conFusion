fbad0afd6139d0fef31ffb74d0e5d8e61b2e2d56
Trianglium <jbrockerrudow@gmail.com>
2022-03-19 09:44:35 -0500

    Login functionality

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -12,6 +12,7 @@ class Header extends Component {
         };
         this.toggleNav = this.toggleNav.bind(this);
         this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
       }
 
       toggleNav() {
@@ -25,6 +26,13 @@ class Header extends Component {
           isModalOpen: !this.state.isModalOpen
         });
       }
+      handleLogin(event) {
+        this.toggleModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value
+            + " Remember: " + this.remember.checked);
+        event.preventDefault();
+
+    }
 
     render() {
         return(
