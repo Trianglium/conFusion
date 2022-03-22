5a677a1229c2f57d253c835b9f077fbcc5554641
Trianglium <jbrockerrudow@gmail.com>
2022-03-22 08:48:09 -0500

    Re Structuring flow of menu / detail components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label, Col, Row } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Form, FormFeedback, FormGroup, Input, Label, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
@@ -42,8 +42,8 @@ const minLength = (len) => (val) => val && (val.length >= len);
                             </li>
                         );
                     })}
-
                 </ul>
+                <CommentForm dishId={dishId} addComment={addComment} />
             </div>
         );
       }
@@ -74,8 +74,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
                         <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                    <RenderComments comments={props.comments} addComment={props.addComment} dishId={props.dish.id} />
-                        <CommentForm dishId={dishId} addComment={addComment} />
+                        <RenderComments comments={props.comments} addComment={props.addComment} dishId={props.dish.id} />
                     </div>
                 </div>
                 </div>
@@ -92,12 +91,12 @@ class CommentForm extends Component{
 		super(props);
 
 		this.state={
+      isNavOpen: false,
 			isModalOpen: false
 		};
 
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.toggleModal = this.toggleModal.bind(this);
-    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
 	}
 
 	handleSubmit(values) {
@@ -118,7 +117,7 @@ class CommentForm extends Component{
 				<Button outline color="secondary"  onClick={this.toggleModal}>
 	                <span className="fa fa-pencil fa-lg"></span> Submit Comment
 	            </Button>
-				<Modal isOpen={this.state.isModalOpen} toggle = {this.toggleModal}>
+				          <Modal isOpen={this.state.isModalOpen} toggle = {this.toggleModal}>
 	                	<ModalHeader toggle = {this.toggleModal}> Submit Comment </ModalHeader>
 	                	<ModalBody>
                     <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
