a3e5d6de2910a2a255dc4c8d174e3a7f102cfad8
Trianglium <jbrockerrudow@gmail.com>
2022-03-26 09:18:03 -0500

    Migrating from addComment to postComment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -30,7 +30,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
         }
     }
 
-    function RenderComments({comments, addComment, dishId}) {
+    function RenderComments({comments, postComment, dishId}) {
       if(comments != null) {
         return (
             <div className="col-12 m-1 text-left">
@@ -45,7 +45,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
                         );
                     })}
                 </ul>
-                <CommentForm dishId={dishId} addComment={addComment} />
+                <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         );
       }
@@ -94,7 +94,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
                         <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} addComment={props.addComment} dishId={props.dish.id} />
+                        <RenderComments comments={props.comments} postComment={props.postComment} dishId={props.dish.id} />
                     </div>
                 </div>
                 </div>
@@ -121,7 +121,7 @@ class CommentForm extends Component{
 
 	handleSubmit(values) {
       this.toggleModal();
-      this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+      this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
   toggleModal(){
