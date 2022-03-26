6653c04a0df68692edee500297cb119e6586947f
Trianglium <jbrockerrudow@gmail.com>
2022-03-26 08:59:15 -0500

    Route dish detail to use photos from api server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -13,7 +14,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
             return (
                 <div className="col-12 m-1 text-left">
                     <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
                             <CardTitle><h5>{dish.name}</h5></CardTitle>
                             <CardText><p>{dish.description}</p></CardText>
