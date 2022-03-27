52938fc44de3ed62ecd2dfec0f72f156cd922315
Trianglium <jbrockerrudow@gmail.com>
2022-03-27 05:59:24 -0500

    About Component using api leader data & Rendering animations

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderLeader({leader}) {
     if(leader != null) {
       return(
         <div key={leader.id} className="col-12 mt-5">
-          <Media tag="li">
-              <Media left middle>
-                  <Media object src={leader.image} alt={leader.name} />
-              </Media>
-              <Media body className="ml-5">
-                  <Media heading>{leader.name}</Media>
-                  <p>{leader.designation}</p>
-                  <p>{leader.description}</p>
-              </Media>
-          </Media>
+          <Stagger in>
+            <Media tag="li">
+              <Fade in>
+                <Media left middle>
+                    <Media object src={leader.image} alt={leader.name} />
+                </Media>
+                <Media body className="ml-5">
+                    <Media heading>{leader.name}</Media>
+                    <p>{leader.designation}</p>
+                    <p>{leader.description}</p>
+                </Media>
+              </Fade>
+            </Media>
+          </Stagger>
         </div>
       );
     }
