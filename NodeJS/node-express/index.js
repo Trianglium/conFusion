036e2e48adc0642abd3f84b65d359507e4023c05
Trianglium <jbrockerrudow@gmail.com>
2022-03-28 07:46:06 -0500

    Comment code & Bug Fixes

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -1,15 +1,32 @@
-const express = require('express'),
-     http = require('http');
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+// Import dishRouter Node Module
+const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const app = express();
-const dishRouter = require('./routes/dishRouter');
 
+const app = express();
+
+app.use(morgan('dev'));
 app.use(bodyParser.json());
+
+// Mount dishRouter
 app.use('/dishes', dishRouter);
+
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+
+});
+
+
+// REST API METHODS
 
 app.all('/dishes', (req,res,next) => {
   res.statusCode = 200;
@@ -53,8 +70,8 @@ app.delete('/dishes/:dishId', (req, res, next) => {
     res.end('Deleting dish: ' + req.params.dishId);
 });
 
-const server = http.createServer(app);
 
+const server = http.createServer(app);
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
