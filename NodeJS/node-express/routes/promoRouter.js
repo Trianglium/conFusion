d21300cff244d52b6d3f133455d22326db021be6
Trianglium <jbrockerrudow@gmail.com>
2022-03-28 07:46:26 -0500

    promoId endpoint implementation

--- NodeJS/node-express/routes/promoRouter.js
+++ NodeJS/node-express/routes/promoRouter.js
@@ -26,4 +26,31 @@ promoRouter.route('/')
     res.end('Deleting all promotions');
 });
 
+
+// promoId
+promoRouter.route('/:promoId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+
+.get((req,res,next) => {
+    res.end('Will send details of the promo: ' + req.params.promoId +' to you!');
+});
+
+.post((req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /promotions/'+ req.params.promoId);
+});
+
+.put((req, res, next) => {
+  res.write('Updating the promo: ' + req.params.promoId + '\n');
+  res.end('Will update the promo: ' + req.body.name +
+        ' with details: ' + req.body.description);
+});
+
+.delete((req, res, next) => {
+    res.end('Deleting promo: ' + req.params.promoId);
+});
+
 module.exports = promoRouter;
