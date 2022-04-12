60cda980e966fe8cf68864471eb007ddd41c0413
Trianglium <jbrockerrudow@gmail.com>
2022-04-12 04:39:57 -0500

    Add cors to endpoints

--- NodeJS/conFusionServer/routes/promoRouter.js
+++ NodeJS/conFusionServer/routes/promoRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('./cors');
 
 // Integrate Promotion Schema
 const Promotions = require('../models/promotions');
