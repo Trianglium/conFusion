5246262b36c807fc54c0614e594774a22ca0a5ad
Trianglium <jbrockerrudow@gmail.com>
2022-04-13 08:10:06 -0500

    Facebook login fixed

--- NodeJS/conFusionServer/config.js
+++ NodeJS/conFusionServer/config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: 'Your Client App ID',
-        clientSecret: 'Your Client App Secret'
+        clientId: '3088066164785574',
+        clientSecret: '7a095ecc88c56a808a6280697ee86ef1'
     }
 }
